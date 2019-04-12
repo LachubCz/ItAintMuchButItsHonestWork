4746e45e3b2417b28410a7dffef320b6a2d6cb11
LachubCz <petr.genius.buchal@gmail.com>
2019-04-12 20:51:12 +0200

    Update classificator_network.py

--- src/classificator_network.py
+++ src/classificator_network.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Input, Dense, Flatten, BatchNormalization, Activation, PReLU, add, MaxPooling2D, Conv2D
 
 from tools import parse_data
-
+from batch import DataSet
 img_shape = (960, 960)
 
 def classificator_model():
@@ -50,33 +50,36 @@ def strip_futilities(data):
 
 if __name__ == '__main__':
     random.seed(datetime.now())
-    batch_size = 8
-    trn_data_part = 0.7
-    trn_data = parse_data("./data/ground_truths_develop.csv", "./data/images/", "./data/ground_truths/")
-    tst_data = []
-    number_of_tst_data = int(len(trn_data)*(1-trn_data_part))
-    print("Len trn_data: {}; Len trn_data: {}" .format(len(trn_data)-number_of_tst_data, number_of_tst_data))
+    batch_size = 64
+    tst_data = parse_data("./data/ground_truths_develop.csv", "./data/images/", "./data/ground_truths/")
+    trn_data = DataSet(tst_data)
+    tst_data = tst_data[:64]
+    print(len(tst_data))
 
-    tst_indexes = random.sample(range(len(trn_data)), number_of_tst_data)
-    tst_indexes.reverse()
+    #number_of_tst_data = int(len(trn_data)*(1-trn_data_part))
+    #print("Len trn_data: {}; Len trn_data: {}" .format(len(trn_data)-number_of_tst_data, number_of_tst_data))
 
-    for i, item in enumerate(tst_indexes):
-        tst_data.append(trn_data[i])
-        del trn_data[i]
+    #tst_indexes = random.sample(range(len(trn_data)), number_of_tst_data)
+    #tst_indexes.reverse()
 
-    trn_images, trn_labels = strip_futilities(trn_data)
+    #for i, item in enumerate(tst_indexes):
+    #    tst_data.append(trn_data[i])
+    #    del trn_data[i]
+
+    #trn_images, trn_labels = strip_futilities(trn_data)
     tst_images, tst_labels = strip_futilities(tst_data)
 
     model = classificator_model()
     for i in range(10000):
-        trn_indexes = random.sample(range(len(trn_labels)), batch_size)
-        tst_indexes = random.sample(range(len(tst_labels)), batch_size)
+        #trn_indexes = random.sample(range(len(trn_labels)), batch_size)
+        #tst_indexes = random.sample(range(len(tst_labels)), batch_size)
 
-        trn_img_batch = np.take(trn_images, trn_indexes, axis=0)
-        trn_lbl_batch = np.take(trn_labels, trn_indexes, axis=0)
+        #trn_img_batch = np.take(trn_images, trn_indexes, axis=0)
+        #trn_lbl_batch = np.take(trn_labels, trn_indexes, axis=0)
 
-        tst_img_batch = np.take(tst_images, tst_indexes, axis=0)
-        tst_lbl_batch = np.take(tst_labels, tst_indexes, axis=0)
+        #tst_img_batch = np.take(tst_images, tst_indexes, axis=0)
+        #tst_lbl_batch = np.take(tst_labels, tst_indexes, axis=0)
 
+        trn_img_batch, trn_lbl_batch = trn_data.getBatch(batch_size)
         model.fit(trn_img_batch, trn_lbl_batch, epochs=1, batch_size=batch_size, verbose=1,
-                  validation_data=(tst_img_batch, tst_lbl_batch))
+                  validation_data=(tst_images, tst_labels))

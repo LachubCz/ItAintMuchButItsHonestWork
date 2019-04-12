982dc7d383e33bd964734233b3bbd12a7aa45642
LachubCz <petr.genius.buchal@gmail.com>
2019-04-12 11:40:52 +0200

    Data loading added.

--- src/image.py
+++ src/image.py
@@ -1,12 +1,20 @@
 class Image(object):
-    def __init__(self, filename, gt_ellipse_center_x, gt_ellipse_center_y, gt_ellipse_majoraxis, 
-                     gt_ellipse_minoraxis, gt_ellipse_angle, image_width, image_height, category):
+    def __init__(self, image, ground_truths, filename, gt_ellipse_center_x, gt_ellipse_center_y, 
+                 gt_ellipse_majoraxis, gt_ellipse_minoraxis, gt_ellipse_angle, image_width, 
+                 image_height, category):
+        self.image = image
+        self.ground_truths = ground_truths
         self.filename = filename
-        self.gt_ellipse_center_x = gt_ellipse_center_x
-        self.gt_ellipse_center_y = gt_ellipse_center_y
-        self.gt_ellipse_majoraxis = gt_ellipse_majoraxis
-        self.gt_ellipse_minoraxis = gt_ellipse_minoraxis
-        self.gt_ellipse_angle = gt_ellipse_angle
-        self.image_width = image_width
-        self.image_height = image_height
-        self.category = category
+        if gt_ellipse_center_x == '':
+            self.ellipse = False
+        else:
+            self.ellipse = True
+            self.gt_ellipse_center_x = float(gt_ellipse_center_x)
+            self.gt_ellipse_center_y = float(gt_ellipse_center_y)
+            self.gt_ellipse_majoraxis = float(gt_ellipse_majoraxis)
+            self.gt_ellipse_minoraxis = float(gt_ellipse_minoraxis)
+            self.gt_ellipse_angle = float(gt_ellipse_angle)
+
+        self.image_width = int(image_width)
+        self.image_height = int(image_height)
+        self.category = int(category)
--- src/tools.py
+++ src/tools.py
@@ -3,7 +3,7 @@ import cv2
 
 from image import Image
 
-def parse_data(filename, images_path):
+def parse_data(filename, data_path, ground_truths_path):
     with open(filename) as f:
         content = f.readlines()
 
@@ -14,9 +14,15 @@ def parse_data(filename, images_path):
         if i == 0:
             continue
         parametres = item.split(',')
-        img = Image(parametres[0], parametres[1], parametres[2], parametres[3], 
+        image = cv2.imread(os.path.join(data_path, parametres[0]))
+        ground_truth = cv2.imread(os.path.join(ground_truths_path, parametres[0]))
+        img = Image(image, ground_truth, 
+                    parametres[0], parametres[1], parametres[2], parametres[3], 
                     parametres[4], parametres[5], parametres[6], parametres[7], 
                     parametres[8])
         data.append(img)
 
     return data
+
+if __name__ == '__main__':
+    parse_data("./data/ground_truths_develop.csv", "./data/images/", "./data/ground_truth/")

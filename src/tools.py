14ad07b2dbc0badd4391af54b3a643082fcf979f
Martin Ivanco <ivancom.fr@gmail.com>
2019-04-12 15:30:24 +0200

    Loading processed image and ground truth

--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import numpy as np
 
 from image import Image
 
@@ -14,9 +15,19 @@ def parse_data(filename, data_path, ground_truths_path):
         if i == 0:
             continue
         parametres = item.split(',')
-        image = cv2.imread(os.path.join(data_path, parametres[0]))
-        ground_truth = cv2.imread(os.path.join(ground_truths_path, parametres[0]))
-        img = Image(image, ground_truth, 
+
+        image = cv2.imread(os.path.join(data_path, parametres[0]), -1)
+        image_processed = image * np.uint16(65535.0 / max(image.ravel()))
+
+        ground_truth = cv2.imread(os.path.join(ground_truths_path, parametres[0][:parametres[0].rfind('.')] + ".png"), -1)
+        ground_truth_processed = np.copy(ground_truth)
+        indices = np.where(np.any(ground_truth_processed != [0, 0, 255], axis = -1))
+        ground_truth_processed[indices] = [0, 0, 0]
+        indices = np.where(np.all(ground_truth_processed == [0, 0, 255], axis = -1))
+        ground_truth_processed[indices] = [255, 255, 255]
+        ground_truth_processed = cv2.cvtColor(ground_truth_processed, cv2.COLOR_BGR2GRAY)
+        
+        img = Image(image, image_processed, ground_truth, ground_truth_processed,
                     parametres[0], parametres[1], parametres[2], parametres[3], 
                     parametres[4], parametres[5], parametres[6], parametres[7], 
                     parametres[8])
@@ -25,4 +36,4 @@ def parse_data(filename, data_path, ground_truths_path):
     return data
 
 if __name__ == '__main__':
-    parse_data("./data/ground_truths_develop.csv", "./data/images/", "./data/ground_truth/")
+    parse_data("./data/ground_truths_develop.csv", "./data/images/", "./data/ground_truths/")

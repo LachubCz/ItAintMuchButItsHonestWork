95d4ca010ff891f7560a6a259f1292293654f829
vlada-cz <xjerab21@stud.fit.vutbr.cz>
2019-04-13 08:20:39 +0200

    Tohole jsou komentáře pro batch.py

--- src/batch.py
+++ src/batch.py
@@ -1,4 +1,16 @@
-#!/usr/bin/python3
+#################################################################################
+# Description:  Class for generating batch data for training 
+#               clasification neural network
+#               
+# Authors:      Petr Buchal         <petr.buchal@lachub.cz>
+#               Martin Ivanco       <ivancom.fr@gmail.com>
+#               Vladimir Jerabek    <jerab.vl@gmail.com>
+#
+# Date:     2019/04/13
+# 
+# Note:     This source code is part of project created on UnIT HECKATHON
+#################################################################################
+
 
 import numpy as np
 from image import Image
@@ -12,13 +24,21 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot
 
-
+#Class storing data for augmentation
 class DataSet(object):
     def __init__(self, ImageList):
         self.Images = ImageList
         self.p_images, self.labels, self. grand_truths = DataSet.strip_futilities(ImageList)
         random.seed(datetime.now())
 
+
+    #Function for augmentation images
+    #@param: imageArr   - is numpy array of images of shape (n, 1, height, width)
+    #                                                        ^-- number of images
+    #@param: grandArr   - is numpy array of grand truths images of shape (n, 1, height, width)
+    #@param: batchSize  - is integer, which is less or equal 'n'
+    #@return: new_img   - array of augmented images of shape (batchSize, 1, height, width)
+    #@return: new_g_t   - array of augmented grand truths images of shape (batchSize, 1, height, width)
     @staticmethod
     def augmentImage(imageArr, grandArr, batchSize):
         
@@ -38,7 +58,7 @@ class DataSet(object):
                                 zoom_range=0.2,
                                 horizontal_flip=True,
                                 vertical_flip=True)
-
+        #creating Image generator same for image data and grand truths data
         img_dataGen = ImageDataGenerator(**data_gen_args)
         g_tr_dataGen = ImageDataGenerator(**data_gen_args)
 
@@ -52,7 +72,13 @@ class DataSet(object):
 
         return new_img, new_g_t
         
-        
+    #Function for striping unneeded data from Image class
+    #@param: data   - List of Image class       ^-- for more info see image.py
+    #@return: images        - numpy array of images of shape (n, 1, height, width)      %note: 16-bit grayscale
+    #@return: labels        - numpy array of booleans of shape (n,1), 
+    #                           - 0 mean, that i-th image is doesn't contain ellipse
+    #                           - 1 mean, that i-th image contains ellipse
+    #@return: grand_truths  - numpy array of images of shape (n, 1, height, width)      %note: 16-bit grayscale
     @staticmethod
     def strip_futilities(data):
         images = []
@@ -65,7 +91,8 @@ class DataSet(object):
 
         return np.array(images), np.array(labels), np.array(grand_truths)
 
-
+    #Method for getting augmented data for training neural netowrk
+    #@param batchSize   - nuber of images in one training epoche
     def getBatch(self, batchSize, isClassNet=True):
         img, g_truths = DataSet.augmentImage(self.p_images, self.grand_truths, batchSize)
         labels = []

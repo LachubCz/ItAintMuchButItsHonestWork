8582b9647f6a12a5e2205d309101e122c4b54843
Martin Ivanco <ivancom.fr@gmail.com>
2019-04-13 07:29:04 +0200

    ellipse drawing fail

--- src/fit_ellipse.py
+++ src/fit_ellipse.py
@@ -22,7 +22,7 @@ def fitting_error(contour):
     
     # its a line! exterminate it!
     # print(min(bounding_rect[1]) / 2.0)
-    if cumulative_diff < 25 - math.log(min(bounding_rect[1]) * 10):
+    if cumulative_diff < 25 - math.log(min(bounding_rect[1]) * 16):
         # print(min(bounding_rect[1]) / 2.0)
         return 100000
 
@@ -76,37 +76,66 @@ def recursive_contour_divide(contour):
     
     return None
 
-def fit_ellipse(original, segmented, file_to_open = None):
-    # find contours
-    # _, contours, _ = cv2.findContours(segmented, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # windows shit
-    contours, _ = cv2.findContours(segmented, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # this is how we do it
-    #print("cont:", np.shape(contours))
+def sweet_mother_ellipse(image):
+    # image = cv2.cvtColor(np.uint8(np.clip(image, 0, 255)), cv2.COLOR_GRAY2BGR)
+    # base params
+    old_rect = ((0, 0), (0, 0), 0)
+    reduced_contour = None
+    fitted = False
 
-    # get max length contour
-    new_cont = np.array([])
-    if len(contours) > 0:
-        new_cont = contours[0]
-        for i in range(len(contours)-1):
-            new_cont = np.concatenate((new_cont, contours[i]), axis=0)
-    
-    # no ellipse found
-    if len(new_cont) < 5:
-        # print("Less than five")
+    while not fitted:
+        # find contours
+        #_, contours, _ = cv2.findContours(segmented, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # windows shit
+        contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # this is how we do it
+        #print("cont:", np.shape(contours))
+
+        # get max length contour
+        new_cont = np.array([])
+        if len(contours) > 0:
+            new_cont = contours[0]
+            for i in range(len(contours)-1):
+                new_cont = np.concatenate((new_cont, contours[i]), axis=0)
+
+        # no ellipse found
+        if len(new_cont) < 5:
+            return (None, None)
+
+        # get reduced contour
+        reduced_contour = recursive_contour_divide(new_cont)
+        
+
+        # its bullshit, i did not hit her, i did not. oh hi mark!
+        if reduced_contour is None:
+            return (None, None)
+
+        # fit ellipse
+        bounding_rect = cv2.fitEllipse(reduced_contour)
+
+        # draw ellipse
+        # test = cv2.cvtColor(np.uint8(np.clip(image, 0, 255)), cv2.COLOR_GRAY2BGR)
+        #print(max_length)
+        # print(len(reduced_contour))
+        smaller_rect = ((bounding_rect[0][0], bounding_rect[0][1]), (max(bounding_rect[1][0] - 5, 0) , max(bounding_rect[1][1] - 5, 0)), bounding_rect[2])
+        cv2.ellipse(image, smaller_rect, 255, -1)
+        # cv2.imshow("result", cv2.resize(image, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_AREA))
+        # cv2.waitKey(0)
+
+        # check new ellipse
+        if abs(bounding_rect[0][0] - old_rect[0][0]) < 1.0 and abs(bounding_rect[0][1] - old_rect[0][1]) < 1.0 and abs(bounding_rect[1][0] - old_rect[1][0]) < 1.0 and abs(bounding_rect[1][1] - old_rect[1][1]) < 1.0 and abs(bounding_rect[2] - old_rect[2]) < 1.0:
+            fitted = True
+        old_rect = ((bounding_rect[0][0], bounding_rect[0][1]), (bounding_rect[1][0], bounding_rect[1][1]), bounding_rect[2])
+
+    return reduced_contour, old_rect
+
+def fit_ellipse(original, segmented, file_to_open = None):
+    # fit ellipse
+    reduced_contour, bounding_rect = sweet_mother_ellipse(segmented)
+
+    if bounding_rect is None:
         return None
 
-    # get reduced contour
-    reduced_contour = recursive_contour_divide(new_cont)
-
-    # its bullshit, i did not hit her, i did not. oh hi mark!
-    if reduced_contour is None:
-        # print("Reduced is none")
-        reduced_contour = new_cont
-
-    # fit ellipse
-    bounding_rect = cv2.fitEllipse(reduced_contour)
-
     # draw ellipse
-    test = cv2.cvtColor(np.uint8(np.clip(segmented, 0, 255)), cv2.COLOR_GRAY2BGR)
+    test = cv2.cvtColor(np.uint8(np.clip(original, 0, 255)), cv2.COLOR_GRAY2BGR)
     cv2.drawContours(test, reduced_contour, -1, (0,255,0), 3)
     cv2.ellipse(test, bounding_rect, (0, 0, 255), 5)
 
@@ -153,22 +182,16 @@ if __name__ == '__main__':
             continue
         parametres = item.split(',')
 
+        if i < 10:
+            continue
+
         image = cv2.imread(os.path.join("./data/images/", parametres[0]), -1)
-        maxval = max(image.ravel())
-        new_image =  np.uint8(np.clip(255/maxval * image, 0, 255))
-
-        # threshold
-        k_size = 3
-        kernel = np.ones((k_size, k_size),np.float32) / k_size**2
-        dst = cv2.filter2D(new_image,-1,kernel)
-        # tmpImg = cv2.fastNlMeansDenoisingColored(cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR), h = 5, templateWindowSize = 5, searchWindowSize = 15)
-        # blur = cv2.GaussianBlur(cv2.cvtColor(tmpImg, cv2.COLOR_BGR2GRAY),(5,5),0)
-        blur = cv2.GaussianBlur(dst, (5, 5), 0)
-
-        # blur
-        # ret,thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        ret, thresh = cv2.threshold(blur, 80, 255, 0)
-        thresh = np.uint8(np.clip(thresh, 0, 255))
+        image = image * np.uint16(65535.0 / max(image.ravel()))
+        image = np.uint8(np.clip(255.0 / 65535.0 * image, 0, 255))
+        blur = cv2.bilateralFilter(image, 20, 400, 400)
+        # blur = cv2.GaussianBlur(image, (25, 25), 0)
+        blur = blur * np.uint8(255.0 / max(blur.ravel()))
+        ret, thresh = cv2.threshold(blur, 50, 255, 0)
 
 
         ####################
@@ -211,7 +234,7 @@ if __name__ == '__main__':
         #imwrite("{}.png" .format(i), thresh)
         #cv2.imshow("test", thresh)
         #cv2.waitKey(0)
-        res = fit_ellipse(new_image, thresh, os.path.join("./data/ground_truths/", parametres[0][:parametres[0].rfind('.')] + ".png"))
+        res = fit_ellipse(image, thresh, os.path.join("./data/ground_truths/", parametres[0][:parametres[0].rfind('.')] + ".png"))
         if res is None:
             print(parametres[0])
 
